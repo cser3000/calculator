a461ad815e771843119f7d54acb89b1f1633901c
ryzhov.ik <cser3000@mail.ru>
2023-08-02 14:23:28 +0300

    new button

--- script.js
+++ script.js
@@ -30,11 +30,11 @@ function calculation(n1, n2, z) {
 
 function handlingClick(value) {
     if (Number(value) < 10 && !operationF && (num1[0] || value !== '0')) {
-        num1 = num1 + String(value);
+        num1 += String(value);
         elemOutput.value = num1;
     }
     else if (Number(value) < 10 && (num2[0] || value !== '0')) {
-        num2 = num2 + String(value);
+        num2 += String(value);
         elemOutput.value = num2;
     }
     else if ((value === '/' || value === '+' || value === '-' || value === '*') && !operationF) {
@@ -59,6 +59,8 @@ function handlingClick(value) {
         operation = '';
         operationF = false;
         elemOutput.value = '';
+        fraction1 = false;
+        fraction2 = false;
     }
     else if (value === '+/-') {
         if (!operationF) {
@@ -70,21 +72,68 @@ function handlingClick(value) {
             elemOutput.value = num2;
         }
     }
+    else if (value === '.') {
+        if (!operationF && !fraction1) {
+            num1 += num1 ? '.' : '0.';
+            fraction1 = !fraction1;
+        }
+        else if (!fraction2) {
+            num2 += num2 ? '.' : '0.';
+            fraction2 = !fraction2;
+        }
+    }
 }
 
 function handlingKeydown(e) {
-    let code = e.keyCode
-    if (code >= 48 && code <= 57 && !operationF && (num1[0] || code !== 48)) {
-        num1 = num1 + (code - 48);
+    let key = e.key
+    if (key >= 0 && key <= 9 && !operationF && (num1[0] || key !== 0)) {
+        num1 += key;
         elemOutput.value = num1;
     }
-    else if (code >= 48 && code <= 57 && operationF && (num2[0] || code !== 48)) {
-        num2 = num2 + (code - 48);
+    else if (key >= 0 && key <= 9 && operationF && (num2[0] || key !== 0)) {
+        num2 += key;
         elemOutput.value = num2;
+    }
+    else if (key === '.') {
+        if (!operationF && !fraction1) {
+            num1 += num1 ? '.' : '0.';
+            fraction1 = !fraction1;
+        }
+        else if (!fraction2) {
+            num2 += num2 ? '.' : '0.';
+            fraction2 = !fraction2;
+        }
+    }
+    else if ((key === '/' || key === '+' || key === '-' || key === '*') && !operationF) {
+        operationF = true;
+        operation = key;
+    }
+    else if ((key === '/' || key === '+' || key === '-' || key === '*') && operationF) {
+        num1 = calculation(num1, num2, operation);
+        operation = key;
+        num2 = '';
+        elemOutput.value = num1;
+    }
+    else if (key === "=") {
+        num1 = num2 ? calculation(num1, num2, operation) : num1;
+        elemOutput.value = num1;
+        num2 = '';
+        operationF = false;
+    }
+    else if (key === 'Backspace') {
+        num1 = '';
+        num2 = '';
+        operation = '';
+        operationF = false;
+        elemOutput.value = '';
+        fraction1 = false;
+        fraction2 = false;
     }
 }
 
 let operationF = false;
+let fraction1 = false;
+let fraction2 = false;
 
 let num1 = '';
 let num2 = '';
