7a2c3d6039f6f3e235683a801936bfb4f05779da
ryzhov.ik <cser3000@mail.ru>
2023-08-03 11:59:08 +0300

    добавлена ошибка при делении на 0

--- script.js
+++ script.js
@@ -41,17 +41,26 @@ const zeroing = () => {
     fraction2 = false;
 }
 
+const addingNumbers = (a, v) => {
+    a += String(v);
+    a = String(Number(a));
+    elemOutput.value = a;
+    return a;
+}
+
+const signChange = (a) => {
+    a = String(-Number(a));
+    elemOutput.value = a;
+    return a;
+}
+
 function handlingClick(value) {
     if (Number(value) + 1) {
         if (!operationF) {
-            num1 += String(value);
-            num1 = String(Number(num1));
-            elemOutput.value = num1;
+            num1 = addingNumbers(num1, value);
         }
         else {
-            num2 += String(value);
-            num2 = String(Number(num2));
-            elemOutput.value = num2;
+            num2 = addingNumbers(num2, value);
         }
     }
     else if (value === '/' || value === '+' || value === '-' || value === '*') {
@@ -67,12 +76,13 @@ function handlingClick(value) {
         }
     }
     else if (value === "=" || value === 'Enter') {
-        if (calculation(num1, num2, operation) === "Ошибка") {
+        let tmp = calculation(num1, num2, operation);
+        if (tmp === "Ошибка") {
             zeroing();
             elemOutput.value = "Ошибка";
         }
         else {
-            elemOutput.value = num2 ? calculation(num1, num2, operation) : num1;
+            elemOutput.value = num2 ? tmp : num1;
             num2 = '0';
             fraction2 = false;
             operationF = false;
@@ -83,21 +93,19 @@ function handlingClick(value) {
     }
     else if (value === '+/-') {
         if (!operationF) {
-            num1 = String(-Number(num1));
-            elemOutput.value = num1;
+            num1 = signChange(num1);
         }
         else {
-            num2 = String(-Number(num2));
-            elemOutput.value = num2;
+            num2 = signChange(num2);
         }
     }
     else if (value === '.') {
         if (!operationF && !fraction1) {
-            num1 += num1 ? '.' : '0.';
+            num1 += '.';
             fraction1 = !fraction1;
         }
         else if (!fraction2) {
-            num2 += num2 ? '.' : '0.';
+            num2 += '.';
             fraction2 = !fraction2;
         }
     }
